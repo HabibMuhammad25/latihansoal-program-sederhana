7b6227e442517128fcb57258f7ceef8a9f784983
Muhammad Habib <94332139+HabibMuhammad25@users.noreply.github.com>
2023-10-15 16:40:29 +0700

    Add files via upload

---
+++ Latihan-Soal-Part3.py
@@ -0,0 +1,11 @@
+# Soal Ketiga
+print("=" * 40)
+print("Program Menampilkan Bilangan Kelipatan 3 ")
+print("=" * 40)
+
+nilai = int(input("Masukkan Bilangan : "))
+
+for i in range(1, nilai + 1):
+      print( i * 3, end=" ")
+      
+print()
